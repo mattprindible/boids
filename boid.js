c7634d78e14da7cf427801e41e12b3cb6bfd1fef
Matt Prindible <code@mattprindible.com>
2021-03-24 22:26:20 -0400

    cohesion

--- boid.js
+++ boid.js
@@ -41,14 +41,39 @@ class Boid {
         return steering;
     }
 
+    cohesion(boids) {
+        let perceptionRadius = 50;
+        let steering = createVector();
+        let total = 0;
+        for (let other of boids) {
+            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+            if (other!=this && d<perceptionRadius) {
+                steering.add(other.position);
+                total++;
+            }
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.sub(this.position);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+        }
+        return steering;
+    }
+
     flock(boids) {
         let alignment = this.align(boids);
-        this.acceleration = alignment;
+        let cohesion = this.cohesion(boids);
+        this.acceleration.add(alignment);
+        this.acceleration.add(cohesion);
     }
 
     update() {
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed);
+        this.acceleration.mult(0);
     }
 
     show() {
