5e47d2c0d1aee34a233c971f7a36cb6c7902e774
Matt Prindible <service+github@mattprindible.com>
2021-03-25 22:08:05 -0400

    separation

--- boid.js
+++ boid.js
@@ -62,9 +62,38 @@ class Boid {
         return steering;
     }
 
+    separation(boids) {
+        let perceptionRadius = 50;
+        let steering = createVector();
+        let total = 0;
+        for (let other of boids) {
+            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+            if (other!=this && d<perceptionRadius) {
+                let diff = p5.Vector.sub(this.position, other.position);
+                diff.div(d);
+                steering.add(diff);
+                total++;
+            }
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+        }
+        return steering;
+    }
+
     flock(boids) {
         let alignment = this.align(boids);
         let cohesion = this.cohesion(boids);
+        let separation = this.separation(boids);
+
+        alignment.mult(alignSlider.value);
+        cohesion.mult(cohesionSlider.value);
+        separation.mult(separationSlider.value);
+
+        this.acceleration.add(separation)
         this.acceleration.add(alignment);
         this.acceleration.add(cohesion);
     }
